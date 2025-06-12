27e6b5450c197311b934f0155c2f1550b4525dba
sworna <imsworna@gmail.com>
2025-06-12 03:37:55 +0000

    Created edit, update and destroy method in ideas_controller and added delete button to delete ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,17 +4,37 @@ class IdeasController < ApplicationController
     end
     
     def create 
-        @idea = Idea.create(ideas_params)
+        @idea = Idea.create(idea_params)
+        if @idea.valid?
+            #code
+        else
+            #code
+        end
         redirect_to root_path
     end
 
     def edit
-        @ideas = ideas.find(params[:id])
+        @idea = Idea.find(params[:id])
+    end
+
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(ideas_params)
+            redirect_to root_path
+        else
+            redirect_to edit_idea_path(params[:id])
+        end
+    end
+
+    def destroy
+        @idea = Idea.find(params[:id])
+        @idea.destroy
+        redirect_to root_path
     end
 
     private 
     
-        def ideas_params
+        def idea_params
             params.require(:idea).permit(:description, :author)
         end
 end
